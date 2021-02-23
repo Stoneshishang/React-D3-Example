14284a26dbab3af9cbef602784d3930ee388a187
StoneShiShang <43474457+Stoneshishang@users.noreply.github.com>
2021-02-22 22:32:40 -0500

    write the click counter with Hooks.

--- src/App.js
+++ src/App.js
@@ -1,10 +1,20 @@
-import React, { Component } from 'react';
+import React, { useState } from 'react';
 import Test from './Test';
 
 function App() {
+
+  const [click, setClick] = useState(0);
+
+  const handleClick = () => {
+    setClick(prev => prev + 1);
+  }
+
   return (
     <div className="App">
-      
+      <Test person="Leo" handleClick={handleClick}/>
+      <Test person="Shang" handleClick={handleClick}/>
+      <Test person="Nina" handleClick={handleClick}/>
+      Person Clicks: {click}
     </div>
   );
 }
